7f99ee6944c342081c95f770e561c21e916689d4
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-15 17:34:02 +0500

    seprate route file

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 const http = require("http");
-const fs=require('fs')
+const routes=require('./routes')
 
-const server = http.createServer((req, res) => {
-  
-  fs.appendFileSync("server.logs",`${req.url} \n`)
-  
-console.log(req)
-  if (req.url === "/") {
-    res.write("<h1>Home Page</h1>");
-   return res.end();
-  }else if (req.url === "/about") {
-    res.write("<h1>About Pagee</h1>");
-  return  res.end();
-  }else{
-    res.write("<h1>Page Not Found</h1>");
-    return  res.end();
-  }
-});
+const server = http.createServer(routes)
 
 server.listen(3000, () => {
   console.log("Server started at port : 3000");
