4e7bc8289daeda56b19f01344bf87f6ccbfee241
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-16 13:13:05 +0500

    express installed and server created

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require("http");
-const routes=require('./routes')
-
-const server = http.createServer(routes)
-
-server.listen(3000, () => {
-  console.log("Server started at port : 3000");
-});
+const express=require("express")
+const app=express()
+app.use((req,resp,next)=>{
+resp.send("<h1>Welcome to ExpresJs</h1>")
+})
+ app.listen(3000,()=>{
+  console.log("Servar Started on Port : 3000")
+ })
