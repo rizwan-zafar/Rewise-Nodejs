5c8484b0a516ba099f2e0bd9b51bf72d8e04e6b3
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-17 13:10:53 +0500

    site changed to products from instead for student portal

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const bodyParser=require('body-parser')
-const studentRoutes=require('./routes/student')
+const shopeRoutes=require('./routes/shop')
 const adminRoutes=require('./routes/admin')
 const path=require('path')
 
 app.use(bodyParser.urlencoded({extended:false}))
-
-app.get('/', (req, resp, next) => {
-  resp.send("<h1>Welcome to ExpresJsss</h1>");
-});
-
-app.use("/student",studentRoutes);
+app.use("/",shopeRoutes);
 app.use('/admin',adminRoutes); //i have added '/admin' so now in url we need to add just /admin/routeinadmin
-app.use((req,resp,next)=> resp.status(404).sendFile(path.join(__dirname,'./','views','404.html')))
+app.use((req,resp,next)=> resp.status(404).sendFile(path.join(__dirname,'views','404.html')))
 
 
 
