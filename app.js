b37ca7ad024b1febde0a9be2488cc8d243b0d143
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-17 15:14:10 +0500

    seprate css file and load statics folder

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const bodyParser=require('body-parser')
 const shopeRoutes=require('./routes/shop')
 const adminRoutes=require('./routes/admin')
 const path=require('path')
-
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))  // used to import static folder like public - now we are able to access css files in pages
+
+
 app.use("/",shopeRoutes);
 app.use('/admin',adminRoutes); //i have added '/admin' so now in url we need to add just /admin/routeinadmin
 app.use((req,resp,next)=> resp.status(404).sendFile(path.join(__dirname,'views','404.html')))
