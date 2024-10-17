1d316d8ebc1b1f850aead3d2263364f80be1aa5c
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-17 13:28:02 +0500

    inline style Moved to Style tag in head

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express=require('express')
 const router=express.Router()
-
+const path=require('path')
 router.get('/',(req,resp,next)=>{
     resp.send("<h1>Admin Home Page?</h1>")
 })
 
-router.get('/all',(req,resp,next)=>{
-    resp.send("<h1>all admin list?</h1>")
-})
+router.get("/add-product", (req, resp, next) => {
+    resp.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  });
 
 module.exports =router
--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/add-product", (req, resp, next) => {
-  resp.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-});
+
 
 router.get("/", (req, resp, next) => {
   resp.sendFile(path.join(__dirname, "../", "views", "shop.html")); //    ../vews/shop.html
