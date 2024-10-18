5e649e9cb2c6e3f137197ac540fa4ac95991862f
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-18 18:41:52 +0500

    if conditioin added to hbs shop page

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ const adminData=require('./admin')
   
 
 router.get("/", (req, resp, next) => {
-   resp.render('shop',{products:adminData.products,docTitle:'Shops'})
+   resp.render('shop',{products:adminData.products,docTitle:'Shops',hasProdcuts: adminData.products.length > 0})
   // resp.sendFile(path.join(__dirname, "../", "views", "shop.html")); //    ../vews/shop.html
 });
 
