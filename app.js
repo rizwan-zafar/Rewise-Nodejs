16382a43995cd69d3a09101c596b6a12e3042534
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-18 16:49:28 +0500

    setting handlebars and rednring 404page as handlebars

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+
 const bodyParser=require('body-parser')
-const shopeRoutes=require('./routes/shop')
-const adminRoutes=require('./routes/admin')
 const path=require('path')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname,'public')))  // used to import static folder like public - now we are able to access css files in pages
 
 
+//setting Handlebar
+const {engine}=require('express-handlebars')
+app.engine('hbs', engine({ extname: '.hbs',defaultLayout:false})); // note: then name that we are using here will be extenstion of file like 404.hbs
+app.set('view engine', 'hbs')  //settitng hbs as default engine
+app.set('views','views')  //setting views as views
+
+
+const shopeRoutes=require('./routes/shop')
+const adminRoutes=require('./routes/admin')
+
+//routes
 app.use("/",shopeRoutes);
 app.use('/admin',adminRoutes); //i have added '/admin' so now in url we need to add just /admin/routeinadmin
-app.use((req,resp,next)=> resp.status(404).sendFile(path.join(__dirname,'views','404.html')))
+app.use((req,resp,next)=> resp.status(404).render('404',{docTitle:"Page not Found"}))
 
 
 
