0367bfd0ff25427bd0571a16a9223c0df229be52
rizwan-zafar <morzafar.c@stc.com.sa>
2024-10-19 12:27:52 +0500

    other pages converted to ejs + shop page dynamic for proudcts

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,8 @@ const adminData=require('./admin')
   
 
 router.get("/", (req, resp, next) => {
-  //  resp.render('shop.html',{products:adminData.products,docTitle:'Shops'})
-  resp.sendFile(path.join(__dirname, "../", "views", "shop.html")); //    ../vews/shop.html
+   resp.render('shop',{products:adminData.products,docTitle:'Shops'})
+  // resp.sendFile(path.join(__dirname, "../", "views", "shop.html")); //    ../vews/shop.html
 });
 
 module.exports = router;
